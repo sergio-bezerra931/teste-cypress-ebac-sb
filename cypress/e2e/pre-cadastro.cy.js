bddca14e67fda406757232f3be3126939d8fa7a5
sergio bezerra <ssbezerra45@gmail.com>
2025-11-20 20:13:16 -0300

    alteraçao no projeto

---
+++ cypress/e2e/pre-cadastro.cy.js
@@ -0,0 +1,18 @@
+///<reference types="cypress" />
+import { faker } from '@faker-js/faker';
+describe('Funcionalidade Pre cadastro', () => {
+    before(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+    it('Deve completar o pre cadastro com sucesso', () => {
+        cy.get('#reg_email').type(faker.internet.email())
+        cy.get('#reg_password').type('!teste@teste#$')
+        cy.get(':nth-child(4) > .button').click()
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+
+        cy.get('#account_first_name').type('sergio')
+        cy.get('#account_last_name').type('bezerra')
+        cy.get('.woocommerce-Button').click()
+        cy.get('.woocommerce-message').should('contain','Detalhes da conta modificados com sucesso.')
+    });
+});
